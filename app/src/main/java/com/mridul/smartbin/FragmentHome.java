241108f305df511062b0ca1b84f8c5596f828aa8
Avinash <playingavi@gmail.com>
2017-04-20 03:23:18 +0530

    Make Home Screen UI ang Buttons

--- app/src/main/java/com/mridul/smartbin/FragmentHome.java
+++ app/src/main/java/com/mridul/smartbin/FragmentHome.java
@@ -3,21 +3,28 @@ package com.mridul.smartbin;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import static com.mridul.smartbin.BackgroundWorkerLoginActivity.CURRENT_USER_EMAIL;
 
 
-public class FragmentHome extends Fragment {
+public class FragmentHome extends Fragment implements  View.OnClickListener{
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
     }
+
+    ImageView filled_bins ;
+    ImageView driver_mgmt ;
+    ImageView path_maker ;
+    ImageView notification ;
 
     @Nullable
     @Override
@@ -31,6 +38,53 @@ public class FragmentHome extends Fragment {
         BackgroundWorkerAccountInfo backgroundWorker = new BackgroundWorkerAccountInfo(getContext());
         backgroundWorker.execute(type, CURRENT_USER_EMAIL);
 
+        filled_bins = (ImageView) v.findViewById(R.id.filled_bins_home);
+        driver_mgmt = (ImageView) v.findViewById(R.id.driver_management_home);
+        path_maker = (ImageView) v.findViewById(R.id.path_maker_home);
+        notification = (ImageView) v.findViewById(R.id.notifiction_home);
+
+        filled_bins.setOnClickListener(this);
+        driver_mgmt.setOnClickListener(this);
+        path_maker.setOnClickListener(this);
+        notification.setOnClickListener(this);
+
         return v;
     }
+
+    @Override
+    public void onClick(View v) {
+
+        Fragment fragment = null;
+        switch (v.getId()) {
+            case R.id.filled_bins_home:
+                fragment = new FragmentBinMarkersFilledBins();
+                replaceFragment(fragment);
+                break;
+
+            case R.id.driver_management_home:
+                fragment = new FragmentDriverManagement();
+                replaceFragment(fragment);
+                break;
+            case R.id.path_maker_home:
+                fragment = new FragmentPathMaker();
+                replaceFragment(fragment);
+                break;
+
+            case R.id.notifiction_home:
+                fragment = new FragmentNotifications();
+                replaceFragment(fragment);
+                break;
+        }
+    }
+
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_after_login1, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+
 }
